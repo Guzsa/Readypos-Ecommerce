5c5b7a7a2b6f4afb02f6f08da9609772b7599b81
Santiago <santiguzman1336@gmail.com>
2026-04-04 02:27:31 -0300

    fix: sincronizado base path con el nombre real del repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Readypos-Ecommerce/", // El nombre de tu repositorio en GitHub
+  base: "/Readypos-Ecommerce/", // <--- Respetando la R y la E mayúsculas
 })
