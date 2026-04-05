192ca81acffc72bc69ab32cc6dd94c21c6422c6d
Santiago <santiguzman1336@gmail.com>
2026-04-05 20:33:54 -0300

    fix: deploy desde carpeta dist en main

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Readypos-Ecommerce/", // INDISPENSABLE para que Git encuentre los assets
+  base: "/Readypos-Ecommerce/",
 })
