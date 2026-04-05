50e5c4ce3cbee1db7a999b99884ec5b836b544f2
Santiago <santiguzman1336@gmail.com>
2026-04-04 23:25:29 -0300

    Revertir a versión estable

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- CAMBIALO POR ESTO (Punto y barra)
+  base: "/Readypos-Ecommerce/", 
 })
