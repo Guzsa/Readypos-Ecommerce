bbbd7cca0330733931083f27addd55649c078d6a
Santiago <santiguzman1336@gmail.com>
2026-04-04 23:23:50 -0300

    fix: base path to relative

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Readypos-Ecommerce/", // <--- Respetando la R y la E mayúsculas
+  base: './', // <--- CAMBIALO POR ESTO (Punto y barra)
 })
